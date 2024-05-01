d1faeba24ce431acde7409b51cde7fe45f0bebbd
Marco Huber <marco.huber00@gmail.com>
2024-05-01 20:36:54 +0100

    Changed Meili Config

--- config/plugins.js
+++ config/plugins.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
   meilisearch: {
     config: {
       // Your meili host
-      host: "http://localhost:7700",
+      host: env('MEILI_API_ENDPOINT', ''),
       // Your master key or private key
       apiKey: env('MEILI_MASTER_KEY', ''),
     }
