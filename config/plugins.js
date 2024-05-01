f1dd7b15c9e1dce75bba972f69d9dd063ef92e72
Marco Huber <marco.huber00@gmail.com>
2024-05-01 18:17:54 +0100

    Added Compose File

--- config/plugins.js
+++ config/plugins.js
@@ -4,14 +4,14 @@
 // file: config/plugins.js
 "use strict";
 
-module.exports = () => ({
+module.exports = ({ env }) => ({
   // ...
   meilisearch: {
     config: {
       // Your meili host
       host: "http://localhost:7700",
       // Your master key or private key
-      apiKey: "6LKw9o4P0yjivfuOSHy_TKckH2sPdTK9rIz5nVkt9eI",
+      apiKey: env('MEILI_MASTER_KEY', ''),
     }
   }
   // ...
